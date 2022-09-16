1b5e35852441f3ec4d7de753b59cfd01d14d1459
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-16 18:02:46 +0300

    l

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -21,17 +21,18 @@ keymap("n", "<leader>e", ":Lex 30<cr>", opts)
 --FZF
 keymap("n", "<leader>f", ":FZF ~<cr>", opts)
 
---Go to definition
-vim.keymap.set("n", "<space>d", vim.lsp.buf.implementation, opts)
+-- --Go to definition
+-- vim.keymap.set("n", "<space>d", vim.lsp.buf.implementation, opts)
 
 keymap("n", "<leader>q", ":q<cr>", opts)
 keymap("n", "<leader>wq", ":wq<cr>", opts)
 keymap("n", "<leader>w", ":w<cr>", opts)
 
-keymap("n", "<leader-Right>", ":vertical resize -10<CR>", opts)
-keymap("n", "<leader-Left>", ":vertical resize +10<CR>", opts)
+-- keymap("n", "<leader>l", ":vertical resize -10<CR>", opts)
+-- keymap("n", "<leader>h", ":vertical resize +10<CR>", opts)
 
 keymap("n", "<leader>t", ":tabedit<cr>", opts)
+-- keymap("n", "<leader>g", ":TermExec cmd='clear' size=60<cr>", opts)
 
 --Indent Mode
 keymap("v", "<", "<gv", opts)
@@ -50,7 +51,7 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
--- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
--- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
--- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
--- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
