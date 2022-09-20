a7537c3450ad820746dcedbb9abf870037df88dc
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-20 19:55:25 +0300

    l

---
+++ lua/user/lsp.lua
@@ -0,0 +1,5 @@
+require'lspconfig'.clangd.setup{}
+require'lspconfig'.java_language_server.setup{}
+require'lspconfig'.pyright.setup{}
+require'lspconfig'.pyre.setup{}
+require'lspconfig'.tsserver.setup{}
