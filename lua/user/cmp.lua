4a58dcf3c3435b91afb9669c3b2065e34e452eb7
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-09-20 19:59:59 +0300

    l

---
+++ lua/user/cmp.lua
@@ -0,0 +1,53 @@
+require("nvim-autopairs").setup {}
+require('Comment').setup()
+
+local cmp = require "cmp"
+
+cmp.setup {
+  mapping = {
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping {
+      i = cmp.mapping.confirm { select = true },
+    },
+    ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }, { "i" }),
+    ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }, { "i" }),
+  },
+  experimental = {
+    ghost_text = true,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  sources = {
+    -- 'crates' is lazy loaded
+    { name = "nvim_lsp" },
+    { name = "path" },
+    {
+      name = "buffer",
+      option = {
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end,
+      },
+    },
+    { name = "spell" },
+  },
+  formatting = {
+    fields = {
+      cmp.ItemField.Abbr,
+      cmp.ItemField.Kind,
+      cmp.ItemField.Menu,
+    },
+}
+
+}
+-- insert `(` after select function or method item
+local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
+
