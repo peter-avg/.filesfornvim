85f7d75496fc58075e3b8f48cff0cf5721738b8e
Peter Avgerinos <108369084+PeterAvgerinos@users.noreply.github.com>
2022-10-15 22:38:59 +0300

    l

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -5,4 +5,30 @@ require'lspconfig'.pyre.setup{}
 require'lspconfig'.tsserver.setup{}
 require'lspconfig'.cssls.setup{}
 require'lspconfig'.html.setup{}
-require'lspconfig'.rust_analyzer.setup{}
+local nvim_lsp = require'lspconfig'
+
+local on_attach = function(client)
+    require'completion'.on_attach(client)
+end
+
+nvim_lsp.rust_analyzer.setup({
+    on_attach=on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
